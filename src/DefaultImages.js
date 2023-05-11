e35ce051967f00c514b46a49a73b8650a76c8eba
Thanam Pangri <tpangri@gmail.com>
2023-05-11 23:28:23 +0200

    Update DefaultImages.js

--- src/DefaultImages.js
+++ src/DefaultImages.js
@@ -15,7 +15,7 @@ const colorURLImage = 'https://raw.githubusercontent.com/Azure-Samples/cognitive
 const mixedMultiPagePDFURL = 'https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/MultiPageHandwrittenForm.pdf';
 const domainURLImage = 'https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg';
 const typeURLImage = 'https://raw.githubusercontent.com/Azure-Samples/cognitive-services-python-sdk-samples/master/samples/vision/images/make_things_happen.jpg';*/
-const azureStoragePic = 'https://dasistmeinstorageac.blob.core.windows.net/pictures/le_sauze_du_lac_mountains_island.jpg';
+const azureStoragePic = 'https://dasistmeinstorageac.blob.core.windows.net/pictures/active_check.jpg';
 
 const DefaultImages = [
     /*describeURL,
